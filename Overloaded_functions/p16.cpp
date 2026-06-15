cc45a9d704eabc90891d2edf8fe462fd939ada4c
student-nithin <ninsai2007@gmail.com>
2026-06-15 15:13:32 +0530

    new files

--- Overloaded_functions/p16.cpp
+++ Overloaded_functions/p16.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "p16.h"
+#include <iostream>
+
+using namespace std;
+
+int add(int num1,int num2) {
+    return num1 + num2;
+}
+
+int add(int num1,int num2,int num3) {
+    return num1+num2+num3;
+}
+
+int add(int num1,int num2,int num3,int num4) {
+    return num1+num2+num3+num4;
+}
+
+
+int main() {
+
+
+    cout << add(1,2);
+    cout << add(1,2,3);
+    cout << add(1,2,3,4);
+
+    return 0;
+}
