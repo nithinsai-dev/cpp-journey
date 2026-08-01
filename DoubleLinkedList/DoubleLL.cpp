988c7fed8b9c27628d28ed0b5c69a0e648cfa68d
student-nithin <ninsai2007@gmail.com>
2026-08-01 23:07:37 +0530

    new files added

---
+++ DoubleLinkedList/DoubleLL.cpp
@@ -0,0 +1,5 @@
+//
+// Created by NITHIN SAI on 28-07-2026.
+//
+
+#include "DoubleLL.h"
---
+++ DoubleLinkedList/DoubleLL.h
@@ -0,0 +1,16 @@
+//
+// Created by NITHIN SAI on 28-07-2026.
+//
+
+#ifndef C_DOUBLELL_H
+#define C_DOUBLELL_H
+
+
+
+class DoubleLL {
+
+};
+
+
+
+#endif //C_DOUBLELL_H
